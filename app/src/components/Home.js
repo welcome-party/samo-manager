e2981064181d1d832244182f0e058a548aefd913
Sachin Goyal <sachin@fanduel.com>
2021-12-31 16:17:27 +0000

    updated to solana

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Home
+            Hi! Welcome to Solana.
+            This app makes it easier for you to invite your friends to Solana Block chain.
+            Go ahead and "Send voucher" to someone with their email address. A voucher is only valid for 5 days.
+            Once you have sent the voucher, recipient will get an email with a link.
+            Once they have accepted the voucher (in the timeframe) you will receive confirmation with their Solana wallet Public Key. 
+            After that you can transfer tokens to them.
         </div>
     );
 };
