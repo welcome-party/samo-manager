ca4ad0313e39d693b557fef438abba4f8546fa70
Sachin Goyal <sachin@fanduel.com>
2022-01-04 01:55:13 +0000

    list

--- app/src/components/ListVouchers.js
+++ app/src/components/ListVouchers.js
@@ -1,134 +1,80 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useState } from "react";
+
+import { Connection, PublicKey } from '@solana/web3.js';
+import {
+    Program, Provider, web3
+} from '@project-serum/anchor';
+import idl from '../idl/samo_manager.json';
+
+import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
+import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import './ListVouchers.css';
 
-const ListVouchers = () => {
+const clusterUrl = process.env.REACT_APP_CLUSTER_URL;
+
+const wallets = [
+    /* view list of available wallets at https://github.com/solana-labs/wallet-adapter#wallets */
+    getPhantomWallet()
+]
+
+const { SystemProgram, Keypair } = web3;
+
+/* create an account  */
+const voucherAccount = Keypair.generate();
+
+const opts = {
+    preflightCommitment: "processed"
+}
+const programID = new PublicKey(idl.metadata.address);
+
+function ListVouchers() {
+    const wallet = useWallet();
+    const history = useHistory();
+    const [vouchers, setVouchers] = useState([]);
+
+    async function getProvider() {
+        const connection = new Connection(clusterUrl, opts.preflightCommitment);
+
+        const provider = new Provider(
+            connection, wallet, opts.preflightCommitment,
+        );
+        return provider;
+    }
+
+    async function listVouchers(event) {
+        event.preventDefault();
+
+        const provider = await getProvider()
+        const program = new Program(idl, programID, provider);
+
+        try {
+            setVouchers(await program.account.voucher.all());
+        } catch (err) {
+            console.log("Transaction Error: ", err);
+            alert('Tranaction Error:' + err);
+            history.push('/home');
+        }
+    }
+
     return (
         <div className='Content'>
-            ListVouchers
+            <button onClick={listVouchers}>Fetch All</button>
+            {vouchers.map((voucher, key) => <div key= {key}>{voucher.account.fromEmail}</div>)}
         </div>
     );
-};
+}
 
-export default ListVouchers;
+const ListVouchersWithProvider = () => (
+    <ConnectionProvider endpoint={clusterUrl}>
+        <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+                <ListVouchers />
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+)
 
-// import './App.css';
-// import React, { Component }  from 'react';
-// import { useState } from 'react';
-// import { Connection, PublicKey } from '@solana/web3.js';
-// import {
-//   Program, Provider, web3
-// } from '@project-serum/anchor';
-// import idl from './idl.json';
-
-// import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
-// import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
-// import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-
-// const wallets = [
-//   /* view list of available wallets at https://github.com/solana-labs/wallet-adapter#wallets */
-//   getPhantomWallet()
-// ]
-
-// const { SystemProgram, Keypair } = web3;
-// /* create an account  */
-// const baseAccount = Keypair.generate();
-// const opts = {
-//   preflightCommitment: "processed"
-// }
-// const programID = new PublicKey(idl.metadata.address);
-
-// function App() {
-//   const [value, setValue] = useState(null);
-//   const wallet = useWallet();
-
-//   async function getProvider() {
-//     /* create the provider and return it to the caller */
-//     /* network set to local network for now */
-//     const network = "http://127.0.0.1:8899";
-//     const connection = new Connection(network, opts.preflightCommitment);
-
-//     const provider = new Provider(
-//       connection, wallet, opts.preflightCommitment,
-//     );
-//     return provider;
-//   }
-
-//   async function createCounter() {
-//     const provider = await getProvider()
-//     /* create the program interface combining the idl, program ID, and provider */
-//     const program = new Program(idl, programID, provider);
-//     try {
-//       /* interact with the program via rpc */
-//       await program.rpc.create({
-//         accounts: {
-//           baseAccount: baseAccount.publicKey,
-//           user: provider.wallet.publicKey,
-//           systemProgram: SystemProgram.programId,
-//         },
-//         signers: [baseAccount]
-//       });
-
-//       const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-//       console.log('account: ', account);
-//       setValue(account.count.toString());
-//     } catch (err) {
-//       console.log("Transaction error: ", err);
-//     }
-//   }
-
-//   async function increment() {
-//     const provider = await getProvider();
-//     const program = new Program(idl, programID, provider);
-//     await program.rpc.increment({
-//       accounts: {
-//         baseAccount: baseAccount.publicKey
-//       }
-//     });
-
-//     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-//     console.log('account: ', account);
-//     setValue(account.count.toString());
-//   }
-
-//   if (!wallet.connected) {
-//     /* If the user's wallet is not connected, display connect wallet button. */
-//     return (
-//       <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
-//         <WalletMultiButton />
-//       </div>
-//     )
-//   } else {
-//     return (
-//       <div className="App">
-//         <div>
-//           {
-//             !value && (<button onClick={createCounter}>Create counter</button>)
-//           }
-//           {
-//             value && <button onClick={increment}>Increment counter</button>
-//           }
-
-//           {
-//             value && value >= Number(0) ? (
-//               <h2>{value}</h2>
-//             ) : (
-//               <h3>Please create the counter.</h3>
-//             )
-//           }
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// const AppWithProvider = () => (
-//   <ConnectionProvider endpoint="http://127.0.0.1:8899">
-//     <WalletProvider wallets={wallets} autoConnect>
-//       <WalletModalProvider>
-//         <App />
-//       </WalletModalProvider>
-//     </WalletProvider>
-//   </ConnectionProvider>
-// )
-
-// export default AppWithProvider;
+export default ListVouchersWithProvider;
