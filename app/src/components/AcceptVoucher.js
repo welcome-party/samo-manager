0234b3e99cffc42ad1ddac3fd96a00f2380ff75d
Sachin Goyal <sachin@fanduel.com>
2022-01-09 21:34:11 +0000

    fetch voucher

--- app/src/components/AcceptVoucher.js
+++ app/src/components/AcceptVoucher.js
@@ -1,14 +1,89 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Connection, PublicKey } from '@solana/web3.js';
+import {
+    Program, Provider
+} from '@project-serum/anchor';
+import idl from '../idl/samo_manager.json';
+
+import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
+import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+
 import './AcceptVoucher.css';
 import WelcomePartyInfo from './subcomponents/WelcomePartyInfo.js';
 
-const AcceptVoucher = () => {
+const clusterUrl = process.env.REACT_APP_CLUSTER_URL;
+
+const wallets = [
+    /* view list of available wallets at https://github.com/solana-labs/wallet-adapter#wallets */
+    getPhantomWallet()
+]
+
+const opts = {
+    preflightCommitment: "processed"
+}
+const programID = new PublicKey(idl.metadata.address);
+
+function AcceptVoucher() {
+    const search = useLocation().search;
+    const voucherAccount = new URLSearchParams(search).get('voucherAccount');
+
+    const wallet = useWallet();
+    const [voucher, setVoucher] = useState([]);
+
+    async function getProvider() {
+        const connection = new Connection(clusterUrl, opts.preflightCommitment);
+
+        const provider = new Provider(
+            connection, wallet, opts.preflightCommitment,
+        );
+        return provider;
+    }
+
+    async function getVoucher() {
+        const provider = await getProvider()
+        const program = new Program(idl, programID, provider);
+        try {
+            const voucherReturned = await program.account.voucher.fetch(new PublicKey(voucherAccount));
+            if (voucherReturned) {
+                // TODO logic for voucher expiry
+                setVoucher(voucherReturned);
+            }
+        } catch (err) {
+            console.log("Transaction Error: ", err);
+            setVoucher(null);
+        }
+    }
+
+    useEffect(() => {
+        getVoucher();
+    }, []);
+
+
     return (
         <div className='content'>
             <WelcomePartyInfo />
-            <div className='input-area'></div>
+            {
+                !voucher && <div className='input-area'>Invalid Voucher</div>
+            }
+            {
+                voucher && <div className='input-area'> Valid Voucher {voucher.fromName}</div>
+            }
+
         </div>
     );
-};
+}
 
-export default AcceptVoucher;
+const AcceptVoucherWithProvider = () => (
+    <ConnectionProvider endpoint={clusterUrl}>
+        <WalletProvider wallets={wallets}>
+            <WalletModalProvider>
+                <AcceptVoucher />
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+)
+
+export default AcceptVoucherWithProvider;
