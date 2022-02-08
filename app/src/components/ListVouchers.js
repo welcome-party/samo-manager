941cf632acd8ed8bf65519ba774a03b71a9ad6ee
Sachin Goyal <sachin@fanduel.com>
2022-02-08 18:39:03 +0000

    cancel works

--- app/src/components/ListVouchers.js
+++ app/src/components/ListVouchers.js
@@ -6,6 +6,9 @@ import { Connection, PublicKey } from '@solana/web3.js';
 import {
     Program, Provider
 } from '@project-serum/anchor';
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+import * as anchor from '@project-serum/anchor';
+
 import idl from '../idl/samo_manager.json';
 
 import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
@@ -14,6 +17,7 @@ import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-r
 import './ListVouchers.css';
 
 const clusterUrl = process.env.REACT_APP_CLUSTER_URL;
+const mintPublicKey = process.env.REACT_APP_SAMO_MINT ? new PublicKey(process.env.REACT_APP_SAMO_MINT) : new PublicKey("7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU");
 const wallets = [getPhantomWallet()];
 const opts = { preflightCommitment: "processed" };
 const programID = new PublicKey(idl.metadata.address);
@@ -38,7 +42,37 @@ function ListVouchers() {
     }
 
     async function cancelVoucher(voucherKey) {
-        alert(voucherKey);
+        const connection = new Connection(clusterUrl, opts.preflightCommitment);
+        const provider = new Provider(connection, wallet, opts.preflightCommitment);
+        const program = new Program(idl, programID, provider);
+
+        const voucherAccount = await program.account.voucherAccount.fetch(voucherKey);
+        const mintToken = new Token(connection, mintPublicKey, TOKEN_PROGRAM_ID);
+        const vaultAccountSeed = new Uint8Array(voucherAccount.vaultAccountSeed);
+        const vaultAuthoritySeed = anchor.utils.bytes.utf8.encode("voucher");
+
+        try {
+            const senderTokenAccount = await mintToken.getOrCreateAssociatedAccountInfo(provider.wallet.publicKey);
+            const [vaultAccountPda, _vaultAccountBump] = await PublicKey.findProgramAddress([Buffer.from(vaultAccountSeed)], program.programId);
+            const [vaultAuthorityPda, _vaultAuthorityBump] = await PublicKey.findProgramAddress([Buffer.from(vaultAuthoritySeed)],program.programId);
+          
+            await program.rpc.cancelVoucher(
+                {
+                    accounts: {
+                        sender: provider.wallet.publicKey,
+                        senderTokenAccount: senderTokenAccount.address,
+                        vaultAccount: vaultAccountPda,
+                        vaultAuthority: vaultAuthorityPda,
+                        voucherAccount: new PublicKey(voucherKey),
+                        tokenProgram: TOKEN_PROGRAM_ID,
+                    }
+                }
+            );
+        } catch (err) {
+            console.log("Transaction Error: ", err);
+            alert('Tranaction Error:' + err);
+            history.push('/');
+        }
     }
 
     useEffect(() => {
@@ -55,7 +89,7 @@ function ListVouchers() {
                     $SAMO: {voucher.account.tokenCount.toString()} &nbsp;
                     {
                         wallet.connected && (wallet.publicKey.toString() === voucher.account.senderKey.toString()) &&
-                        <input type="button" disabled={!wallet.connected} className='cancel-voucher-button medium-text' value='Cancel' onClick={() => cancelVoucher(voucher.publicKey.toString())}/>
+                        <input type="button" disabled={!wallet.connected} className='cancel-voucher-button medium-text' value='Cancel' onClick={() => cancelVoucher(voucher.publicKey.toString())} />
                     }
                 </div>)}
             </div>
